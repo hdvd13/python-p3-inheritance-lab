7794abee12ca19575b57bd8fe5ac36035453e9cb
Hossein <h.dvd13@gmail.com>
2023-07-21 16:28:01 -0700

    python-p3-inheritance-lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,21 @@ from user import User
 
 import random
 
+knowledge = [
+    "Python function call definition",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "str is a data type in Python",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge=[None]):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
